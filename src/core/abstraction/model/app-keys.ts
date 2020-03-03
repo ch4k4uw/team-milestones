c8c76f0213e82fdcc672779afcbcb87bafe8b538
Pedro Motta <pedro.motta@db1.com.br>
2020-03-03 08:49:02 -0300

    Removed unecessary app-keys fields

--- src/core/abstraction/model/app-keys.ts
+++ src/core/abstraction/model/app-keys.ts
@@ -1,26 +1,5 @@
 import { IFirebaseConfig } from "./firebase-config";
 
 export interface IAppKeys {
-    google: {
-        client_id: string;
-    };
-    facebook: {
-        app_id: string;
-    };
-    owlufin: {
-        aws: {
-            region: string;
-            smtp: {
-                server: string;
-                port: number;
-                useTls: boolean;
-                user: string;
-                password: string;
-                source: string;
-            }
-        }
-    };
     firebase: IFirebaseConfig;
-    droidApiKey: string;
-    webApiKey: string;
 }
